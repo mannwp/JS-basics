29fb052c82c1f776645546f0eea596d0c99011d8
Mann Patel <mannpatel3118@gmail.com>
2025-01-22 15:49:14 +0530

    Add example of destructuring assignment for objects and arrays

---
+++ Destructuring_assignment.js
@@ -0,0 +1,12 @@
+//obj
+const obj = { a: 1, b: 2 };
+// const { a, b } = obj;
+const { a, b } = { a: 1, b: 2 };
+const { a: a1, b: b1 } = obj;
+console.log(a, b);
+console.log(a1, b1);
+
+//arr
+const x = [1, 2, 3];
+const [y, z] = x;
+console.log(y, z);
